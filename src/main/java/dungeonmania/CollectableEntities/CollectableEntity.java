ceed7b74586e2749a821c6f2b9aaeb639134b9af
z5309034 <z5309034@cse.unsw.edu.au>
2021-10-26 12:50:29 +1100

    Stub for CollectableEntity class for testing

---
+++ src/main/java/dungeonmania/CollectableEntities/CollectableEntity.java
@@ -0,0 +1,9 @@
+package dungeonmania.CollectableEntities;
+
+
+// public abstract class CollectableEntity extends Entity - add in later
+public abstract class CollectableEntity {
+
+    // Stub for CollectableEntity...
+
+}
