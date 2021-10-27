4da3fa4945ebf2200ce47cd4b1edd28a718a9c56
z5309034 <z5309034@cse.unsw.edu.au>
2021-10-27 14:23:41 +1100

    Added simple stub constructor for utility

--- src/main/java/dungeonmania/CollectableEntities/Utility.java
+++ src/main/java/dungeonmania/CollectableEntities/Utility.java
@@ -1,7 +1,17 @@
 package dungeonmania.CollectableEntities;
 
+import dungeonmania.util.Position;
+
 public abstract class Utility extends CollectableEntity {
 
-    // Stubs for Utility class...
+    /**
+     * Constructor for Utility
+     * @param id
+     * @param type
+     * @param pos
+     */
+    public Utility(String id, String type, Position pos) {
+        super(id, type, pos);
+    }
 
 }
