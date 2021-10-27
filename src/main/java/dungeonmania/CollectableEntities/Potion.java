60e8f34fec309ba1326b55b46383a05ca730eae2
z5309034 <z5309034@cse.unsw.edu.au>
2021-10-27 14:25:31 +1100

    Added simple methods for Potion class

--- src/main/java/dungeonmania/CollectableEntities/Potion.java
+++ src/main/java/dungeonmania/CollectableEntities/Potion.java
@@ -1,7 +1,27 @@
 package dungeonmania.CollectableEntities;
 
+import dungeonmania.MovingEntities.Player;
+import dungeonmania.util.Position;
+
 public abstract class Potion extends CollectableEntity {
 
-    // Stubs for Potion class...
+    private Player player;
+    /**
+     * Constructor for Potion
+     * @param id
+     * @param type
+     * @param pos
+     */
+    public Potion(String id, String type, Position pos) {
+        super(id, type, pos);
+    }
 
+    // Getters and Setters
+    public void setPlayer(Player plyr) {
+        player = plyr;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
 }
