95961abe81ee10bc7a9683df964809448453ceaf
Jihao Yu <sam@DESKTOP-TTJV2D6.localdomain>
2021-10-27 12:10:05 +1100

    [Moved all test json folder under resources, and changed getJsonFile to accomodate for testing json files]

--- src/main/java/dungeonmania/DungeonManiaController.java
+++ src/main/java/dungeonmania/DungeonManiaController.java
@@ -1,7 +1,9 @@
 package dungeonmania;
 
 import dungeonmania.exceptions.InvalidActionException;
+import dungeonmania.gamemap.GameMap;
 import dungeonmania.response.models.DungeonResponse;
+import dungeonmania.response.models.ItemResponse;
 import dungeonmania.util.Direction;
 import dungeonmania.util.FileLoader;
 
@@ -45,22 +47,35 @@ public class DungeonManiaController {
     }
 
     /**
-     * Given a file path, it will get the json file and return it as
-     * a json object.
+     * Given a file name it will go to the source folder and locate dungeon map,
+     * and if not found it will go into the test tolder to locate the test json 
+     * file and return it as a json object.
      * @return Dungeon Map as JsonObject
      */
-    public JsonObject getJsonFile(String filePath) {
+    public JsonObject getJsonFile(String fileName) {
         // "src\\main\\resources\\dungeons\\" + dungeonName + ".json"
         try {
-            String path = filePath;
-            return JsonParser.parseReader(new FileReader(path)).getAsJsonObject();
+            return JsonParser.parseReader(new FileReader("src\\main\\resources\\dungeons\\" + fileName + ".json")).getAsJsonObject();
         } catch (Exception e) {
-            throw new IllegalArgumentException("File does not exist.");
+            try {
+                return JsonParser.parseReader(new FileReader("src\\test\\java\\dungeonmania\\json_test_files\\" + fileName + ".json")).getAsJsonObject();
+            } catch (Exception r) {
+                throw new IllegalArgumentException("File not found.");
+            }
         }
     }
 
     public DungeonResponse newGame(String dungeonName, String gameMode) throws IllegalArgumentException {
-        return null;
+        if (!getGameModes().contains(gameMode)) {
+            throw new IllegalArgumentException("Game mode does not exist.");
+        }
+        // Need to 
+        JsonObject jsonMap = getJsonFile("Nonexistent");
+
+        // ASSIGN THE MAP STORES BY THE CONTROLLER HERE :: JUST FOR TESTING:
+        GameMap map = new GameMap(gameMode, jsonMap);
+
+        return new DungeonResponse("dungeonId", dungeonName, map.mapToListEntityResponse(jsonMap), new ArrayList<ItemResponse>(), new ArrayList<String>(), "goals");
     }
     
     public DungeonResponse saveGame(String name) throws IllegalArgumentException {
