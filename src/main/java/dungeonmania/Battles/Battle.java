a8850d29a123bb4259f5f297e0b9e2debf7b57b9
z5309034 <z5309034@cse.unsw.edu.au>
2021-10-29 00:32:22 +1100

    Added PeacefulState to battle class

--- src/main/java/dungeonmania/Battles/Battle.java
+++ src/main/java/dungeonmania/Battles/Battle.java
@@ -1,6 +1,8 @@
 package dungeonmania.Battles;
 
+import dungeonmania.CollectableEntities.Bow;
 import dungeonmania.MovingEntities.MovingEntity;
+import dungeonmania.MovingEntities.Player;
 
 public class Battle {
 
@@ -8,14 +10,19 @@ public class Battle {
     BattleState InvincibleState;
     BattleState InvisibleState;
     BattleState NormalState;
+    BattleState PeacefulState;
+    BattleState HardState; 
 
     /**
      * Constructor for Battle class
      */
     public Battle() {
-        NormalState = new NormalState();
+        NormalState = new NormalState(this);
+        PeacefulState = new PeacefulState(this);
         InvincibleState = new InvincibleState();
         InvisibleState = new InvisibleState();
+
+        // Need a way of checking difficulty here to set state.
         state = NormalState;
     }
 
@@ -30,6 +37,9 @@ public class Battle {
     public BattleState getNormalState() {
         return NormalState;
     }
+    public BattleState getPeacefulState() {
+        return PeacefulState;
+    }
     public BattleState getInvisibleState() {
         return InvisibleState;
     }
@@ -37,9 +47,22 @@ public class Battle {
         return InvincibleState;
     }
 
-    public void fight(MovingEntity p1, MovingEntity p2) {
+    public void fight(Player p1, MovingEntity p2) {
         state.fight(p1, p2);
     }
 
+    /**
+     * Checks if the player has a bow so that they can make a second attack.
+     * @param p
+     * @return
+     */
+    public Boolean hasBow(Player p) {
+        Bow bow = (Bow) p.getInventory().getItem("Bow");
+        
+        if (bow == null) {
+            return false;
+        }
+        return true;
+    } 
 
 }
