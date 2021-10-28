5fbdab4db8df890435b7fb71a57336d18430d2b1
z5309034 <z5309034@cse.unsw.edu.au>
2021-10-28 21:04:55 +1100

    Removed pickup method to delegate it to Player

--- src/main/java/dungeonmania/CollectableEntities/CollectableEntity.java
+++ src/main/java/dungeonmania/CollectableEntities/CollectableEntity.java
@@ -26,17 +26,5 @@ public abstract class CollectableEntity extends Entity {
         return player;
     }
 
-    /**
-     * Player picks up the item when they walk over it, defeat an enemy that has the
-     * item or builds the item.
-     * @param player
-     */
-    public void pickUp(Player player) {
-        this.setPos(null);
-        this.setPlayer(player);
-        
-        // this.getPlayer().getInventory.add(this);
-    }
-
     abstract public void use();
 }
