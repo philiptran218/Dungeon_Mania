55ba4430a8c21b92fcd2d6fc5ab71e79bca91d4e
z5309034 <z5309034@cse.unsw.edu.au>
2021-10-29 00:32:57 +1100

    Changed first arg to Player

--- src/main/java/dungeonmania/Battles/BattleState.java
+++ src/main/java/dungeonmania/Battles/BattleState.java
@@ -1,9 +1,10 @@
 package dungeonmania.Battles;
 
 import dungeonmania.MovingEntities.MovingEntity;
+import dungeonmania.MovingEntities.Player;
 
 public interface BattleState {
 
-    public void fight(MovingEntity e1, MovingEntity e2);
+    public void fight(Player p1, MovingEntity p2);
 
 }
