f77ce602e05f85670385ba099723732277331c7e
Vincent Wong <vincent.j.wong@student.unsw.edu.au>
2021-10-28 20:55:09 +1100

    Added player inventory atttribute

--- src/main/java/dungeonmania/MovingEntities/Player.java
+++ src/main/java/dungeonmania/MovingEntities/Player.java
@@ -8,16 +8,21 @@ import java.util.Map;
 import dungeonmania.util.Position;
 import dungeonmania.util.Direction;
 import dungeonmania.Entity;
+import dungeonmania.Inventory;
 import dungeonmania.CollectableEntities.CollectableEntity;
 
 
 public class Player extends MovingEntity implements MovingEntitySubject{
     private List<MovingEntityObserver> listObservers = new ArrayList<MovingEntityObserver>();
+    private Inventory inventory = new Inventory(this);
 
     public Player(String id, String type, Position pos){
         super(id, type, pos, 10, 10);
     }
 
+    public void move(Map<Position, List<Entity>> map) {
+
+    }
     public void move(Map<Position, List<Entity>> map, Direction direction) {
         notifyObservers();
         Position newPos = super.getPos().translateBy(direction);        
@@ -26,11 +31,24 @@ public class Player extends MovingEntity implements MovingEntitySubject{
         } else {
             // Do nothing
         }
+        pickUp(map, newPos);
     }
 
     public boolean canPass(Map<Position, List<Entity>> map, Position pos) {
         return map.get(new Position(pos.getX(), pos.getY(), 1)).isEmpty();
     }
+
+    public void pickUp(Map<Position, List<Entity>> map, Position pos) {
+        List<Entity> collectables = map.get(new Position(pos.getX(), pos.getY(), 2));
+        this.inventory.put(collectables.get(0));
+    }
+
+
+    public Inventory getInventory() {
+        return inventory;
+    }
+
+
 
     ////////////////////////////////////////////////////////////////////////////
     // Observer Pattern
