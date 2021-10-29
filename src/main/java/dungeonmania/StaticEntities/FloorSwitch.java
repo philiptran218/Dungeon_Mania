d0305be64a943a5ada50d3247436070fd334fdfa
Gordon Liang <z5310742@ad.unsw.edu.au>
2021-10-29 19:58:54 +1100

    Removed attribute from floor switch

--- src/main/java/dungeonmania/StaticEntities/FloorSwitch.java
+++ src/main/java/dungeonmania/StaticEntities/FloorSwitch.java
@@ -3,19 +3,10 @@ package dungeonmania.StaticEntities;
 import dungeonmania.util.Position;
 
 public class FloorSwitch extends StaticEntity {
-    private boolean triggered = false;
 
     public FloorSwitch(String id, String type, Position pos) {
         super(id, type, pos);
         super.setCanStandOn(true);
         super.setType("switch");
-    }
-    
-    public boolean isTriggered() {
-        return triggered;
-    }
-
-    public void setTriggered(boolean triggered) {
-        this.triggered = triggered;
     }
 }
