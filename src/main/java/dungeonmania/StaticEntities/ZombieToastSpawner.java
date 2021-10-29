d4f35b2c21d1d6c34fa606e576570280e77fd796
Gordon Liang <z5310742@ad.unsw.edu.au>
2021-10-29 22:15:00 +1100

    Added code for interact

--- src/main/java/dungeonmania/StaticEntities/ZombieToastSpawner.java
+++ src/main/java/dungeonmania/StaticEntities/ZombieToastSpawner.java
@@ -1,8 +1,15 @@
 package dungeonmania.StaticEntities;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import dungeonmania.Entity;
+import dungeonmania.MovingEntities.Player;
 import dungeonmania.gamemap.GameState;
 import dungeonmania.gamemap.HardState;
 import dungeonmania.gamemap.StandardState;
+import dungeonmania.util.Direction;
 import dungeonmania.util.Position;
 
 public class ZombieToastSpawner extends StaticEntity {
@@ -18,6 +25,25 @@ public class ZombieToastSpawner extends StaticEntity {
         super.setCanStandOn(false);
         hardState = new HardState();
         standardState = new StandardState();
+    }
+    public Player canSpawnerBeDestroyed(Position entityPosition, Map<Position, List<Entity>> gameMap, boolean isAdjacent, Player playerEntity) {
+        // Add all the directions into a list
+        List <Direction> directions = new ArrayList<Direction>();
+        directions.add(Direction.UP);
+        directions.add(Direction.RIGHT);
+        directions.add(Direction.DOWN);
+        directions.add(Direction.LEFT);
+        for (Direction dir : directions) {
+            // Checks the surrounding positions
+            Position checkPosition = entityPosition.translateBy(dir);
+            Entity entityOnPosition = gameMap.get(checkPosition).get(3);
+            // Checks if the player is in the surrounding positions and gets the player if true
+            if (entityOnPosition != null && entityOnPosition instanceof Player) {
+                isAdjacent = true;
+                playerEntity = (Player) entityOnPosition;
+            }
+        }
+        return playerEntity;
     }
     /**
      * Getter for tickProgress
