50e6e3f9ef00baaae3b43d71093164838ad80910
Jihao Yu <sam@DESKTOP-TTJV2D6.localdomain>
2021-10-29 14:15:20 +1100

    [Updated the exception thrown in tick where if the player does not have the item in the inventory it will throw invalid action exception]

--- src/main/java/dungeonmania/DungeonManiaController.java
+++ src/main/java/dungeonmania/DungeonManiaController.java
@@ -116,22 +116,24 @@ public class DungeonManiaController {
     }
 
     public DungeonResponse tick(String itemUsed, Direction movementDirection) throws IllegalArgumentException, InvalidActionException {
+        // Check if the item is valid.
         if (!getUsableItems().contains(itemUsed)) {
             throw new IllegalArgumentException("Invalid item used.");
         }
-        // Check inventory in item.
-        /*
-        if (gameMap.getPlayer().getInventory().getItem(itemUsed) == null) {
 
-        }*/
+        // Check inventory in item.
+        if (!gameMap.getPlayer().hasItem(itemUsed) && itemUsed != null) {
+            throw new InvalidActionException("Player does not have the item.");
+        }
+        
         // Move the player:
-        gameMap.setObservers();
         gameMap.getPlayer().move(gameMap.getMap(), movementDirection);
 
         // Move all the moving entities by one tick:
         for (MovingEntity e : gameMap.getMovingEntityList()) {
             e.move(gameMap.getMap());
         }
+
         // Return DungeonResponse
         return returnDungeonResponse();
     }
