d269c0d0b7ac6aee005a09ff0b715f4051245930
Jihao Yu <sam@DESKTOP-TTJV2D6.localdomain>
2021-10-31 11:31:00 +1100

    [Changed name of Dungeonmaniaconbtroller test to gameTestsDMC]

--- src/test/java/dungeonmania/GameTestsDMC.java
+++ src/test/java/dungeonmania/GameTestsDMC.java
@@ -12,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 
 
-public class DungeonManiaControllerTest {
+public class GameTestsDMC {
     // Helper:
     public String getUnix() {
         return "" + System.currentTimeMillis();
