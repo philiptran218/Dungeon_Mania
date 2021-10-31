ff131f18d8204e9d01a2cf4fb774d98db12586b9
z5309034 <z5309034@cse.unsw.edu.au>
2021-10-31 21:29:35 +1100

    Added test for battling merc

--- src/test/java/dungeonmania/BattleTest.java
+++ src/test/java/dungeonmania/BattleTest.java
@@ -1,9 +1,12 @@
 package dungeonmania;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 import org.junit.jupiter.api.Test;
 
 import dungeonmania.response.models.DungeonResponse;
 import dungeonmania.response.models.EntityResponse;
+import dungeonmania.util.Direction;
 import dungeonmania.util.Position;
 
 public class BattleTest {
@@ -29,10 +32,35 @@ public class BattleTest {
 
     // Tests for Battles:
 
+    // Testing no weapon combat against a spider
     @Test
     public void basicBattle() {
         
+
+
     }
+
+    // Tests using buildable weapons to kill a mercenary
+    @Test
+    public void testBuildableWeaponsMercenary() {
+        DungeonManiaController newDungeon = new DungeonManiaController();
+        newDungeon.newGame("buildable_battle", "Peaceful");
+
+        newDungeon.tick(null, Direction.RIGHT);
+        newDungeon.tick(null, Direction.RIGHT);
+        newDungeon.tick(null, Direction.RIGHT);
+        newDungeon.tick(null, Direction.RIGHT);
+        newDungeon.tick(null, Direction.RIGHT);
+        newDungeon.tick(null, Direction.RIGHT);
+        newDungeon.tick(null, Direction.RIGHT);
+        newDungeon.build("bow");
+        newDungeon.build("shield");
+        newDungeon.tick(null, Direction.RIGHT);
+        // This movement should fight the mercenary and win
+        DungeonResponse temp = newDungeon.tick(null, Direction.RIGHT);
+        assertFalse(temp.getEntities().stream().anyMatch(e -> e.getType().equals("mercenary")));
+    }
+
 
 
 
