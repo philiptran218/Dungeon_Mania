fabdfbbeb72a5d2df4294d97519b11661d979134
Jihao Yu <sam@DESKTOP-TTJV2D6.localdomain>
2021-11-08 23:57:24 +1100

    [Fixed APP implementation of time turner]

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -122,8 +122,8 @@ public class App {
             return callUsingSessionAndArgument(request, (dmc) -> dmc.interact(request.queryParams("entityId")));
         }, gson::toJson);
 
-        Spark.post("/api/game/rewind", "application/json", (request, response) -> {
-            return callUsingSessionAndArgument(request, (dmc) -> dmc.rewind(request.queryParams("ticks")));
+        Spark.post("/api/game/rewind/", "application/json", (request, response) -> {
+            return callUsingSessionAndArgument(request, (dmc) -> dmc.rewind(Integer.parseInt(request.queryParams("ticks"))));
         }, gson::toJson); 
 
         Scintilla.start();
