e52f17894f3ed9a49c35e52bf4971da8ea441e8e
z5309034 <z5309034@cse.unsw.edu.au>
2021-11-10 00:21:33 +1100

    Added durability to combat items

--- src/main/java/dungeonmania/CollectableEntities/CombatItems.java
+++ src/main/java/dungeonmania/CollectableEntities/CombatItems.java
@@ -6,6 +6,7 @@ import dungeonmania.util.Position;
 
 public abstract class CombatItems extends CollectableEntity {
 
+    private int durability;
     /**
      * Constructor for CombatItems
      * @param id
@@ -15,10 +16,20 @@ public abstract class CombatItems extends CollectableEntity {
     public CombatItems(String id, String type, Position pos) {
         super(id, type, pos);
     }
-    
+  
+    // Override the function toJSONObject in entity
     @Override
     public JSONObject toJSONObject() {
-        return super.toJSONObject();
+         JSONObject obj = super.toJSONObject();
+         obj.put("durability", getDurability());
+         return obj;
     }
-    
+
+    public int getDurability() {
+        return durability;
+    }
+
+    public void setDurability(int num) {
+        this.durability = num;
+    }
 }
