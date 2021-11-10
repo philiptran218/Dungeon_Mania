04f3ad986fd1f25d3d350f1e4c796425bdfef2e7
Jihao Yu <sam@DESKTOP-TTJV2D6.localdomain>
2021-11-10 14:48:40 +1100

    [Fixed load file]

--- src/main/java/dungeonmania/DungeonManiaController.java
+++ src/main/java/dungeonmania/DungeonManiaController.java
@@ -21,6 +21,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
@@ -80,13 +81,10 @@ public class DungeonManiaController {
      */
     public JsonObject getJsonFile(String fileName) {
         try {
-            return JsonParser.parseReader(new FileReader("src\\main\\resources\\dungeons\\" + fileName + ".json")).getAsJsonObject();
+            String jsonString = FileLoader.loadResourceFile("/dungeons/" + fileName + ".json");
+            return new Gson().fromJson(jsonString, JsonObject.class);
         } catch (Exception e) {
-            try {
-                return JsonParser.parseReader(new FileReader("src\\test\\resources\\dungeons\\" + fileName + ".json")).getAsJsonObject();
-            } catch (Exception r) {
                 throw new IllegalArgumentException("File not found.");
-            }
         }
     }
     
