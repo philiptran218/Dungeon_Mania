89b9066aebe1d50a341b39094c9850bbb2c8c2f4
Terrance <z5309029@ad.unsw.edu.au>
2021-11-11 08:33:28 +1100

    Refactored Player Health, can now save and change at runtime depending on difficulty of dungeon

---
+++ src/main/java/dungeonmania/MovingEntities/PlayerFactory.java
@@ -0,0 +1,18 @@
+package dungeonmania.MovingEntities;
+
+import com.google.gson.JsonObject;
+
+import dungeonmania.gamemap.GameState;
+import dungeonmania.util.Position;
+
+public class PlayerFactory {
+    private static final double PLAYER_DAMAGE = 2;
+    public static Player getPlayer (String id, String type, Position pos, JsonObject jsonObj, GameState state) {
+		if (jsonObj.get("health") != null) {
+            double health = jsonObj.get("health").getAsDouble();
+            return new Player(id, type, pos, health, state.getPlayerMaxHealth(),PLAYER_DAMAGE);
+        } else {
+            return new Player(id, type, pos, state.getPlayerMaxHealth(), state.getPlayerMaxHealth(), PLAYER_DAMAGE);
+        }
+	}
+}
