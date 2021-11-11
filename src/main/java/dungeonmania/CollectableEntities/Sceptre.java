ed8e27befe862ea6e3cf6063b5bfde3fdb061105
z5309034 <z5309034@cse.unsw.edu.au>
2021-11-12 00:10:09 +1100

    Use method changed since player manages bribing

--- src/main/java/dungeonmania/CollectableEntities/Sceptre.java
+++ src/main/java/dungeonmania/CollectableEntities/Sceptre.java
@@ -1,10 +1,9 @@
 package dungeonmania.CollectableEntities;
 
+import dungeonmania.MovingEntities.MovingEntity;
 import dungeonmania.util.Position;
 
 public class Sceptre extends CombatItems {
-
-    private static final int INITIAL_DURABILITY = 5;
     
     /**
      * Constructor for Sceptre
@@ -14,31 +13,11 @@ public class Sceptre extends CombatItems {
      */
     public Sceptre(String id, String type, Position pos) {
         super(id, type, pos);
-        this.setDurability(INITIAL_DURABILITY);
-    }
-
-    /**
-     * Reduces the durability if it has been used
-     */
-    public void reduceDurability() {
-        setDurability(getDurability() - 1);
     }
 
     /**
      * Used to interact with enemies.
      */
     public void use() {
-        reduceDurability();
-        checkNoDurability();
-    }
-
-    /**
-     * Check the durability of the sceptre and if durability is zero 
-     * remove it from the inventory.
-     */
-    public void checkNoDurability() {
-        if (getDurability() == 0) {
-            getPlayer().getInventoryList().remove((CollectableEntity)this);
-        }
-    }    
+    }   
 }
