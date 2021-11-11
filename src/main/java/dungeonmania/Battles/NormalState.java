2def42f94fe97fb41ab00b39172ca8c0f6dba0ed
z5309034 <z5309034@cse.unsw.edu.au>
2021-11-11 12:15:04 +1100

    Added new weapons and battles between assassin and hydra

--- src/main/java/dungeonmania/Battles/NormalState.java
+++ src/main/java/dungeonmania/Battles/NormalState.java
@@ -1,6 +1,8 @@
 package dungeonmania.Battles;
 
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.atomic.AtomicMarkableReference;
 
 import dungeonmania.CollectableEntities.*;
 import dungeonmania.MovingEntities.Mercenary;
@@ -32,23 +34,23 @@ public class NormalState implements BattleState {
 
         while (p1.getHealth() > 0 && p2.getHealth() > 0) {
             // Player attacks enemy first
-            healthModifier(p2, damageCalculation(p1), p1.getHealth());
+            healthModifier(p2, damageCalculation(p1, p2), p1.getHealth());
 
             // Performs a second attack
             if (p2.getHealth() > 0 && this.getBattle().hasBow(p1)) {
-                healthModifier(p2, damageCalculation(p1), p1.getHealth());
+                healthModifier(p2, damageCalculation(p1, p2), p1.getHealth());
             }
             
             // Performs an attack if player has an allied mercenary
             for (MovingEntity merc : p1.getBribedMercenaries()) {
                 if (p2.getHealth() > 0) {
-                    healthModifier(p2, damageCalculation(merc), merc.getHealth());
+                    healthModifier(p2, damageCalculation(merc, p2), merc.getHealth());
                 }
             }
 
             // Then enemy attacks player (if the enemy is still alive)
             if (p2.getHealth() > 0) {
-                healthModifier(p1, damageCalculation(p2), p2.getHealth());
+                healthModifier(p1, damageCalculation(p2, p1), p2.getHealth());
             }
             // If player is dead, check for The One Ring
             if (p1.getHealth() <= 0 && p1.hasItem("one_ring")) {
@@ -68,21 +70,33 @@ public class NormalState implements BattleState {
     }
 
     /**
-     * Checks for base dmg, swords and bows that can modify damage.
+     * Checks for base dmg, swords, bows, anduril and midnight armour that can modify damage.
      * @param p1
      */
-    public double damageCalculation(MovingEntity p1) {
-        if (p1 instanceof Player) {
+    public double damageCalculation(MovingEntity p1, MovingEntity p2) {
+        if (p1.getType().equals("player")) {
             Player plyr = (Player) p1;
             double dmg = plyr.getAttackDamage();
             Sword sword = (Sword) plyr.getInventory().getItem("sword");
             Bow bow = (Bow) plyr.getInventory().getItem("bow");
-
+            Anduril anduril = (Anduril) plyr.getInventory().getItem("anduril");
+            MidnightArmour mArmour = (MidnightArmour) plyr.getInventory().getItem("midnight_armour");
             if (sword != null) {
                 dmg += sword.usedInCombat();
             }
             if (bow != null) {
                 dmg += bow.usedInCombat();
+            }
+            if (anduril != null) {
+                if (p2.getType().equals("assassin") || p2.getType().equals("hydra")) {
+                    dmg += (3 * anduril.usedInCombat());
+                }
+                else {
+                    dmg += anduril.usedInCombat();
+                }
+            }
+            if (mArmour != null) {
+                dmg += mArmour.getDamage();
             }
             return dmg;
         }
@@ -93,56 +107,101 @@ public class NormalState implements BattleState {
 
     public void healthModifier(MovingEntity p2, double dmg, double health) {
         double newHealth;
-        if (p2 instanceof Player) {
-            Player plyr = (Player) p2;
-            Shield shield = (Shield) plyr.getInventory().getItem("shield");
-            Armour armour = (Armour) plyr.getInventory().getItem("armour");
-            double multiplier = 1;
-
-            if (shield != null) {
-                multiplier *= shield.usedInCombat();
-            }
-            if (armour != null) {
-                multiplier *= armour.usedInCombat();
-            }
+        if (p2.getType().equals("player")) {
+            double multiplier = playerDamageMultiplier((Player) p2);
             newHealth = p2.getHealth() - ((health * (dmg * multiplier)) / 10);
         }
-        else if (p2 instanceof Mercenary) {
-            Mercenary merc = (Mercenary) p2;
-            Armour armour = merc.getArmour();
-            double multiplier = 1;
-
-            if (armour != null) {
-                multiplier *= armour.getReduceDamage();
-                armour.reduceDurability();
-
-                // Remove armour if it is broken.
-                if (armour.getDurability() == 0) {
-                    merc.setArmour(null);
-                }
-            }
+        else if (p2.getType().equals("mercenary")) {
+            double multiplier = mercenaryDamageMultiplier((Mercenary) p2);
             newHealth = p2.getHealth() - ((health * (dmg * multiplier)) / 5);
         }
-        else if (p2 instanceof ZombieToast) {
-            ZombieToast zombie = (ZombieToast) p2;
-            Armour armour = zombie.getArmour();
-            double multiplier = 1;
-
-            if (armour != null) {
-                multiplier *= armour.getReduceDamage();
-                armour.reduceDurability();
-
-                // Remove armour if it is broken.
-                if (armour.getDurability() == 0) {
-                    zombie.setArmour(null);
+        else if (p2.getType().equals("zombie_toast")) {
+            double multiplier = zombieDamageMultiplier((ZombieToast) p2);
+            newHealth = p2.getHealth() - ((health * (dmg * multiplier)) / 5);
+        }
+        else if (p2.getType().equals("assassin")) {
+            double multiplier = assassinDamageMultiplier((Assassin) p2);
+            newHealth = p2.getHealth() - ((health * (dmg * multiplier)) / 5);
+        }
+        else if (p2.getType().equals("hydra")) {
+            // Damage indicates that anduril is being used
+            if (dmg >= 8) {
+                newHealth = p2.getHealth() - ((health * dmg) / 5);
+            }
+            else {
+                // Simulate 50/50 chance for hydra to gain health
+                Random rand = new Random();
+                if (rand.nextBoolean()) {
+                    newHealth = p2.getHealth() + dmg;
+                }
+                else {
+                    newHealth = p2.getHealth() - ((health * dmg) / 5);
                 }
             }
-            newHealth = p2.getHealth() - ((health * (dmg * multiplier)) / 5);
         }
         else {
             newHealth = p2.getHealth() - ((health * dmg) / 5);
         }
         p2.setHealth(newHealth);
+    }
+
+    public double playerDamageMultiplier(Player player) {
+        Shield shield = (Shield) player.getInventory().getItem("shield");
+        Armour armour = (Armour) player.getInventory().getItem("armour");
+        MidnightArmour mArmour = (MidnightArmour) player.getInventory().getItem("midnight_armour");
+        double multiplier = 1;
+        if (shield != null) {
+            multiplier *= shield.usedInCombat();
+        }
+        if (armour != null) {
+            multiplier *= armour.usedInCombat();
+        }
+        if (mArmour != null) {
+            multiplier *= mArmour.usedInCombat();
+        }
+        return multiplier;
+    }
+
+    public double zombieDamageMultiplier(ZombieToast zombieToast) {
+        Armour armour = zombieToast.getArmour();
+        double multiplier = 1;
+        if (armour != null) {
+            multiplier *= armour.getReduceDamage();
+            armour.reduceDurability();
+            // Remove armour if it is broken.
+            if (armour.getDurability() == 0) {
+                zombieToast.setArmour(null);
+            }
+        }
+        return multiplier;
+    }
+
+    public double mercenaryDamageMultiplier(Mercenary mercenary) {
+        Armour armour = mercenary.getArmour();
+        double multiplier = 1;
+        if (armour != null) {
+            multiplier *= armour.getReduceDamage();
+            armour.reduceDurability();
+            // Remove armour if it is broken.
+            if (armour.getDurability() == 0) {
+                mercenary.setArmour(null);
+            }
+        }
+        return multiplier;
+    }
+
+    public double assassinDamageMultiplier(Assassin assassin) {
+        Armour armour = assassin.getArmour();
+        double multiplier = 1;
+        if (armour != null) {
+            multiplier *= armour.getReduceDamage();
+            armour.reduceDurability();
+            // Remove armour if it is broken.
+            if (armour.getDurability() == 0) {
+                assassin.setArmour(null);
+            }
+        }
+        return multiplier;
     }
 
     public void spawnOneRing(Player p1) {
